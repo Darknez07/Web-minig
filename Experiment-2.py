58ff19adc093cb6dae7b1d58d73ab46184a71ac6
Darknez07 <rishikakkar42@gmail.com>
2022-01-29 00:05:19 +0530

    Edited

--- Experiment-2.py
+++ Experiment-2.py
@@ -1,4 +1,5 @@
 # using nltk package
+
 from nltk.corpus import stopwords
 import string
 import nltk
@@ -6,6 +7,7 @@ from nltk.stem.porter import *
 from collections import Counter
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import wordpunct_tokenize,sent_tokenize
+
 # nltk.download('wordnet')
 # nltk.download('omw-1.4')
 lemmat = WordNetLemmatizer()
@@ -14,19 +16,23 @@ stemmer = PorterStemmer()
 stops = set(stopwords.words('english'))
 stops.add('')
 # Hamlet doc
+
 sentences = sent_tokenize(open('hamlet.txt','r').read().lower())
 tokenized = []
 for x in sentences:
     tokenized+=wordpunct_tokenize(x)
 print(len(tokenized))
 # Tokenize
+
 fl = open('hamlet.txt','r').read().lower().replace('\n',' ').replace('\t','').translate(str.maketrans('','',string.punctuation)).split(' ')
 filtered = [x for x in fl if x not in stops]
 stemmed = [stemmer.stem(x) for x in filtered]
 print(len(filtered))
 print(Counter(stemmed).most_common(10))
+
 # We see stemmer has left somethings out like delivery to deliv
 # Thus lemmatizer
+
 lemmed = [lemmat.lemmatize(x) for x in filtered]
 print(len(lemmed))
 print(Counter(lemmed).most_common(10))
